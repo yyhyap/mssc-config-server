170ae1fa97ef16eca715332413b9083dd07b4d65
yyhyap <yongheng98@hotmail.com>
2021-12-12 14:42:01 +0800

    Initial commit

---
+++ src/test/java/com/yyh/msscconfigserver/MsscConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yyh.msscconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsscConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
